823bd9337c23b54a080be4e88722b5e637008374
Chu-Jun <ongchujun2002@gmail.com>
2023-07-10 20:41:59 +0800

    Comment

--- Assignment 2/src/SearchStation.java
+++ Assignment 2/src/SearchStation.java
@@ -6,8 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-
+// Search station to according to user input
 public class SearchStation {
+
+    // Declare and initialize the variable
     JFrame frame = new JFrame();
     private JButton findStationButton;    
     JButton backButton = new JButton("Back to main page");
@@ -24,25 +26,27 @@ public class SearchStation {
 
     public SearchStation() {
         
+        // Set frame properties
         frame.setResizable(false);
         frame.setTitle("Bus Route Tracking App");
-        
         frame.setSize(745, 400);
         frame.setLayout(new BorderLayout());
         frame.add(headerPanel, BorderLayout.NORTH);
-    
-        centerPanel.setPreferredSize(new Dimension(745, 400));
+        frame.add(bottomPanel, BorderLayout.SOUTH);
         frame.add(centerPanel, BorderLayout.WEST);
+        centerPanel.setPreferredSize(new Dimension(745, 400));
 
+        // Add buttonPanel and footerPanel to bottomPanel
         bottomPanel.add(buttonPanel);
         bottomPanel.add(footerPanel);
-        frame.add(bottomPanel, BorderLayout.SOUTH);
-
+        
+        // Set background colour for all panel
         headerPanel.setBackground(new Color(0x6096B4));
         centerPanel.setBackground(new Color(0xBDCDD6));
         buttonPanel.setBackground(new Color(0xBDCDD6));
         footerPanel.setBackground(new Color(0x6096B4));
         
+        // Set properties of header section
         headerLabel.setText("Bus Route Tracking Application");
         headerLabel.setVerticalAlignment (JLabel.CENTER);
         headerLabel.setHorizontalAlignment(JLabel.RIGHT);
@@ -50,6 +54,7 @@ public class SearchStation {
         headerLabel.setForeground(new Color(0xEEE9DA));
         headerPanel.add(headerLabel);
 
+        // Set properties of the station section
         stationLabel.setText("Station:");
         stationLabel.setHorizontalTextPosition(JLabel.CENTER);
         stationLabel.setVerticalTextPosition(JLabel.BOTTOM);
@@ -61,38 +66,46 @@ public class SearchStation {
         stationTextField.setColumns(20);
         centerPanel.add(stationTextField);
 
+        // Set the properties of the findStationButton
         findStationButton = new JButton("Find Station");
         findStationButton.setFont(new Font("Arial", Font.BOLD,15));
         findStationButton.setBackground (new Color (0x6096B4));
         findStationButton.setForeground (new Color (0xcad2c5));
         buttonPanel.add(findStationButton);
 
+        // Set properties of backButton
         backButton.setFont(new Font("Arial", Font.BOLD,15));
         backButton.setBackground (new Color (0x6096B4));
         backButton.setForeground (new Color (0xcad2c5));
         buttonPanel.add(backButton);
 
+        // Set properties of footer section
         footerLabel.setText("Prepared by Chu Jun & Zee Ching for CPT212 Assignment 2");
         footerLabel.setFont(new Font("Bradley Hand ITC", Font.BOLD, 18)); //set font
         footerLabel.setHorizontalTextPosition(JLabel.LEFT);
         footerLabel.setVerticalTextPosition(JLabel.BOTTOM);
         footerPanel.add(footerLabel);
 
+        // Set frame visibility to true
         frame.setVisible(true);
 
         findStationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 
+                // Get station name from the textfield
                 String Station = stationTextField.getText();
 
+                // Get list of vertex in the graph
                 List<Vertex> list = StartApplication.graph.getVertices();
                 Boolean found = false;
 
+                // Use for loop to check whether the station aready exists in the list of vertex
                 for(int i=0; i<list.size(); i++){
                     Vertex temp = list.get(i);
                     String tempStation = temp.getStationName(0);
                     int results = tempStation.compareToIgnoreCase(Station);
+                    // If already exists, prompt user
                     if(results==0){
                         JOptionPane.showMessageDialog(null,
                         "The station selected exists!",
@@ -101,16 +114,16 @@ public class SearchStation {
                         found = true;
                     }
                 }
+                // If not found, prompt user that the station is not exists
                 if(!found){
                     JOptionPane.showMessageDialog(null,
                     "The station selected does not exists!",
                     "Station Not Found",
                     JOptionPane.INFORMATION_MESSAGE);
                 }
-
-                
             }
         });
+        // Bring user to main page if they click on the backButton
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
